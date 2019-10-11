1938532507a6dd7388b5f6f98b11d329196f78e8
shofiyyahzunaizah <56434050+shofiyyahzunaizah@users.noreply.github.com>
2019-10-11 16:56:06 +0700

    kecepatan tanpa fungsi

---
+++ iyaaa.py
@@ -0,0 +1,12 @@
+nama = 'Latifah Zunairoh'
+program = 'gerak lurus'
+
+print('f program (program) oleh {nama}')
+
+def hitung_kecepatan (jarak, waktu) :
+jarak = 1000
+waktu = 5 * 60
+kecepatan = hitung_kecepatan (jarak / waktu)
+
+print ( f'jarak = {jarak/1000}km ditempuh dalam waktu = {waktu/60}menit')
+print ( f'sehingga kecepatan = {kecepatan} m/s')
